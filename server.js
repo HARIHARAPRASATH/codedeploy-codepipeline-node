0e21e84cc856d63306fa7bc2f899f9090c831350
HARIHARA PRASATH <hari.hari840@gmail.com>
2021-01-13 11:04:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('http')
     res.writeHeader(200,{'Content-Type': 'text/html'})
     res.write('<p>DEVOPS</p>')
     res.write('<p>AWS</p>')
-    res.end('<h1>KAAY LABS PVT LTD </h1>')
+    res.end('<h1>WELCOME TO KAAY LABS PVT LTD </h1>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
